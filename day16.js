e7b39e27fffefbff240a5e7e8165e66bba563420
AkhrorbekAbdurakhimov <akhrorbekabdurakhimov@gmail.com>
2023-12-17 19:46:26 +0500

    minor changes

--- day16.js
+++ day16.js
@@ -7,116 +7,112 @@ let matrix = arr.map(el => el.split(''));
 let result = [...new Array(matrix.length)].map(el => new Array(matrix[0].length).fill('.'))
 
 const partOne = (startR = 0, startC = 0, direction = 'forward') => {
-
-  let isContinue = false;
-
-  if (direction === 'forward') {
-    for (let i = startC; i < matrix.length; i++) {
-      if (matrix[startR][i] === '.') {
-        matrix[startR][i] = '#';
-        result[startR][i] = '#';
-      } else if (matrix[startR][i] === '-')
-        continue;
-      else if (matrix[startR][i] === '|' && !isContinue) {   
-        result[startR][i] = '#';
-        partOne(startR - 1, i, 'upward');
-        partOne(startR + 1, i, 'downward');
-        break;
-      } else if (matrix[startR][i] === '/' && !isContinue) {  
-        result[startR][i] = '#';
-        partOne(startR - 1, i, 'upward');
-        break;
-      } else if (matrix[startR][i] === '\\' && !isContinue) {
-        result[startR][i] = '#';
-        partOne(startR + 1, i, 'downward');
-        break;
-      } else if (result[startR][i] === '#') {
-        isContinue = true;
-      }
+  
+  if (matrix[startR][startC] === '.') {
+    result[startR][startC] = '#';
+    switch (direction) {
+      case 'upward': partOne(startR - 1, startC, 'upward'); break;
+      case 'downward': partOne(startR + 1, startC, 'downward'); break;
+      case 'forward': partOne(startR, startC + 1, 'forward'); break;
+      case 'backward': partOne(startR, startC - 1, 'backward'); break;
     }
   }
 
-  if (direction === 'backward') {
-    for (let i = startC; i >= 0; i--) {
-      if (matrix[startR][i] === '.') {
-        matrix[startR][i] = '#';
-        result[startR][i] = '#';
-      } else if (matrix[startR][i] === '-')
-        continue;
-      else if (matrix[startR][i] === '|' && !isContinue) {
-        result[startR][i] = '#';
-        partOne(startR - 1, i, 'upward');
-        partOne(startR + 1, i, 'downward');
-        break;
-      } else if (matrix[startR][i] === '/' && !isContinue) {
-        result[startR][i] = '#';
-        partOne(startR + 1, i, 'downward');
-        break;
-      } else if (matrix[startR][i] === '\\' && !isContinue) {
-        result[startR][i] = '#';
-        partOne(startR - 1, i, 'upward');
-        break;
-      } else if (result[startR][i] === '#') {
-        isContinue = true;
-      }
-    }
-  }
-
-  if (direction === 'downward') {
-    for (let i = startR; i < matrix[0].length; i++) {
-      if (matrix[i][startC] === '.') {
-        matrix[i][startC] = '#'
-        result[i][startC] = '#'
-      } else if (matrix[i][startC] === '|')
-        continue
-      else if (matrix[i][startC] === '-' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC - 1, 'backward');
-        partOne(i, startC + 1, 'forward');
-        break;
-      } else if (matrix[i][startC] === '/' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC - 1, 'backward');
-        break;
-      } else if (matrix[i][startC] === '\\' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC + 1, 'forward');
-        break;
-      } else if (result[i][startC] === '#') {
-        isContinue = true;
-      }
-    }
-  }
-
-  if (direction === 'upward') {
-    for (let i = startR; i >= 0; i--) {
-      console.log(matrix[i][startC]);
-      if (matrix[i][startC] === '.') {
-        matrix[i][startC] = '#'
-        result[i][startC] = '#'
-      } else if (matrix[i][startC] === '|')
-        continue;
-      else if (matrix[i][startC] === '-' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC - 1, 'backward');
-        partOne(i, startC + 1, 'forward');
-        break;
-      } else if (matrix[i][startC] === '/' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC + 1, 'forward');
-        break;
-      } else if (matrix[i][startC] === '\\' && !isContinue) {
-        result[i][startC] = '#'
-        partOne(i, startC - 1, 'backward');
-        break;
-      } else if (result[i][startC] === '#') {
-        isContinue = true;
-        break;
-      }
-    }
-  }
+  // if (startR >= 0 && startR < matrix.length && startC >= 0 && startC < matrix[0].length) {
+  //   if (direction === 'forward') {
+  //     for (let i = startC; i < matrix.length; i++) {
+  //       if (matrix[startR][i] === '.') {
+  //         result[startR][i] = '#';
+  //       } else if (matrix[startR][i] === '-')
+  //         continue;
+  //       else if (matrix[startR][i] === '|') {   
+  //         result[startR][i] = '#';
+  //         partOne(startR - 1, i, 'upward');
+  //         partOne(startR + 1, i, 'downward');
+  //         break;
+  //       } else if (matrix[startR][i] === '/') {  
+  //         result[startR][i] = '#';
+  //         partOne(startR - 1, i, 'upward');
+  //         break;
+  //       } else if (matrix[startR][i] === '\\') {
+  //         result[startR][i] = '#';
+  //         partOne(startR + 1, i, 'downward');
+  //         break;
+  //       }
+  //     }
+  //   }
+  
+  //   if (direction === 'backward') {
+  //     for (let i = startC; i >= 0; i--) {
+  //       if (matrix[startR][i] === '.') {
+  //         result[startR][i] = '#';
+  //       } else if (matrix[startR][i] === '-')
+  //         continue;
+  //       else if (matrix[startR][i] === '|') {
+  //         result[startR][i] = '#';
+  //         partOne(startR - 1, i, 'upward');
+  //         partOne(startR + 1, i, 'downward');
+  //         break;
+  //       } else if (matrix[startR][i] === '/') {
+  //         result[startR][i] = '#';
+  //         partOne(startR + 1, i, 'downward');
+  //         break;
+  //       } else if (matrix[startR][i] === '\\') {
+  //         result[startR][i] = '#';
+  //         partOne(startR - 1, i, 'upward');
+  //         break;
+  //       }
+  //     }
+  //   }
+  
+  //   if (direction === 'downward') {
+  //     for (let i = startR; i < matrix[0].length; i++) {
+  //       if (matrix[i][startC] === '.') {
+  //         result[i][startC] = '#'
+  //       } else if (matrix[i][startC] === '|')
+  //         continue
+  //       else if (matrix[i][startC] === '-') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC - 1, 'backward');
+  //         partOne(i, startC + 1, 'forward');
+  //         break;
+  //       } else if (matrix[i][startC] === '/') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC - 1, 'backward');
+  //         break;
+  //       } else if (matrix[i][startC] === '\\') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC + 1, 'forward');
+  //         break;
+  //       }
+  //     }
+  //   }
+  
+  //   if (direction === 'upward') {
+  //     for (let i = startR; i >= 0; i--) {
+  //       if (matrix[i][startC] === '.') {
+  //         result[i][startC] = '#'
+  //       } else if (matrix[i][startC] === '|')
+  //         continue;
+  //       else if (matrix[i][startC] === '-') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC - 1, 'backward');
+  //         partOne(i, startC + 1, 'forward');
+  //         break;
+  //       } else if (matrix[i][startC] === '/') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC + 1, 'forward');
+  //         break;
+  //       } else if (matrix[i][startC] === '\\') {
+  //         result[i][startC] = '#'
+  //         partOne(i, startC - 1, 'backward');
+  //         break;
+  //       } 
+  //     }
+  //   }
+  // }
+  
+  fs.writeFileSync('res.txt', result.map(el => el.join('')).join('\n'), 'utf-8')
 }
 
 partOne();
-
-fs.writeFileSync('res.txt', result.map(el => el.join('')).join('\n'), 'utf-8')
